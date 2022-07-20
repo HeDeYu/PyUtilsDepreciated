61fd4b85e7509f9a42be997ed5f282bf3354742a
Deyu He <18565286660@163.com>
2022-07-20 10:17:55 +0800

    feat: add check instance

--- src/pyutils/__init__.py
+++ src/pyutils/__init__.py
@@ -1,6 +1,7 @@
 """Top-level package for PyUtils."""
-from . import file_io
+from . import core, file_io
 from ._version import get_versions
+from .core import *  # noqa: F401, F403
 from .file_io import *  # noqa: F401, F403
 
 __author__ = """Deyu He"""
@@ -11,3 +12,4 @@ del get_versions
 
 __all__ = []
 __all__ += file_io.__all__
+__all__ += core.__all__
